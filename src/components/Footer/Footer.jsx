307e1660e2ac682cbc2f9398041ba935cdbfbb11
yohanvillarp <yohannikel21@gmail.com>
2024-11-23 10:56:42 -0500

    Mejorando estructura inicial

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Contenido del footer
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
-
+import './Header.css';
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+      Contenido del header
+    </header>
   )
 }
 
