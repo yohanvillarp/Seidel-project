32c8f5ba45f0eb905b8d7ff07dfb71daa095c8fc
yohanvillarp <yohannikel21@gmail.com>
2024-12-16 16:20:36 -0500

    modificacion nula

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -6,19 +6,9 @@ const HomeView = () => {
 
 
   return (
-    <div className= "home">
+    <div className="home">
       <div className="home__banner">
         <Banner imgBanner={banner} />
-      </div>
-      <div className='home__promociones'>
-        <h2 className='home__promociones--title'>
-          Promociones
-        </h2>
-
-        <p className='home__promociones--text'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia. Doloremque, voluptas, doloremque, quos, consequuntur, ipsa, dolores, voluptatibus, asperiores, fugiat, laborum, quibusdam, eaque.
-        </p>
-
       </div>
     </div>
   );
